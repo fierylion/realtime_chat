48c72a5102120ac1dbc49cb0cfeb8373296dffcd
fierylion <mawalladaniel2018@gmail.com>
2024-06-25 08:41:57 -0700

    changes

--- realtime_chat/routing.py
+++ realtime_chat/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/room/(?P<room_name>\w+)/$', consumers.RoomConsumers.as_asgi()),
+    re_path(r'ws/room/(?P<room_name>[a-zA-Z0-9_-]+)/$', consumers.RoomConsumers.as_asgi()),
 ]
